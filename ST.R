a7088bd10f4bb1c9b0726b0c65d99b98515c8a76
Aajiayishi <jessieyiup@gmail.com>
2023-09-02 09:21:29 -0400

    Initial commit

--- ST.R
+++ ST.R
@@ -1,1 +1,7 @@
 usethis::create_project("~/Desktop/Aajiayishi.github.io")
+usethis::use_git()
+usethis::git_default_branch_rename(to = "gh-pages")
+usethis::create_github_token()
+access token: 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
+gitcreds::gitcreds_set()
+usethis::use_github()
